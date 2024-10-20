cf211a59079ab27737427c704cd8f1f7a30c378a
Adam <97370646+anon-adam@users.noreply.github.com>
2024-10-20 19:01:30 -0400

    Day 6 Part 2

--- 2019/6.py
+++ 2019/6.py
@@ -18,4 +18,44 @@ def part1():
     print(direct + indirect)
 
 
-part1()
+def part2():
+    adj = {}
+    for m in mappings:
+        orbitee, orbiter = m.split(")")
+        adj[orbiter] = orbitee
+
+    source = adj["YOU"]
+    target = adj["SAN"]
+
+    cur = source
+    path_from_source_to_root = set()
+    while cur != "COM":
+        path_from_source_to_root.add(cur)
+        cur = adj[cur]
+
+    cur = target
+    closest_shared_parent = None
+    while cur != "COM":
+        if cur in path_from_source_to_root:
+            closest_shared_parent = cur
+            break
+        cur = adj[cur]
+
+    cur = source
+    distance_from_source_to_shared_parent = 0
+    while cur != closest_shared_parent:
+        distance_from_source_to_shared_parent += 1
+        cur = adj[cur]
+
+    cur = target
+    distance_from_target_to_shared_parent = 0
+    while cur != closest_shared_parent:
+        distance_from_target_to_shared_parent += 1
+        cur = adj[cur]
+
+    print(distance_from_source_to_shared_parent + distance_from_target_to_shared_parent)
+
+
+if __name__ == "__main__":
+    # part1()
+    part2()
