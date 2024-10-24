1b4f3aca41b991e1e88fc3896e03afe693e11d54
Adam <97370646+anon-adam@users.noreply.github.com>
2024-10-23 23:47:08 -0400

    2021 Day 6 Fix

--- 2021/6.py
+++ 2021/6.py
@@ -1,11 +1,10 @@
 data = open('input/6', 'r').read().split(',')
 data = [int(i) for i in data]
 
-days = 256
-
 state = data
 
-def part1(state, days):
+def part1(state):
+    days = 80
     for _ in range(days):
         print("day " + str(_))
         new_fish = 0
@@ -17,17 +16,16 @@ def part1(state, days):
                 state[idx] -= 1
         for _ in range(new_fish):
             state.append(8)
-        #print("day: " + str(day) + ": " + "state: " + str(state))
 
     print(len(state))
 
-def part2(state, days):
+def part2(state):
+    days = 256
     d = {}
     for _ in range(9):
         d[_] = 0
     for fish in state:
         d[fish] += 1
-    #print("initial state : " + str(d))
     for _ in range(days):
         new_fish = 0
         fish_resets = 0
@@ -39,7 +37,6 @@ def part2(state, days):
                 d[day - 1] = d[day]
         d[8] = new_fish
         d[6] += fish_resets
-        #print("day : " + str(_ + 1) + ": " + "state: " + str(d))
     print(sum([fish for fish in d.values()]))
 
-part2(state, days)
+part2(state)
