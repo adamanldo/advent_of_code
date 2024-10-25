43eee640b0292da7d74b810be1e05ba3961f5f1e
Adam <97370646+anon-adam@users.noreply.github.com>
2024-10-24 23:13:21 -0400

    Fix Day 13

--- 2021/13.py
+++ 2021/13.py
@@ -1,24 +1,24 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
-with open('input/13', 'r') as f:
-    coords, instructions = f.read().split('\n\n')
+with open("input/13", "r") as f:
+    coords, instructions = f.read().split("\n\n")
     parsed_coords = []
-    for cord in coords.split('\n'):
-        for x, y in [cord.split(',')]:
-            parsed_coords.append((int(x), int(y))) 
-    instructions = instructions.split('\n')
+    for cord in coords.split("\n"):
+        for x, y in [cord.split(",")]:
+            parsed_coords.append((int(x), int(y)))
+    instructions = instructions.split("\n")
     parsed_instructions = []
     for instruction in instructions:
         x = instruction.split()[2]
-        y = x.split('=')
+        y = x.split("=")
         parsed_instructions.append((y[0], int(y[1])))
 
 visible_dots = set(parsed_coords)
 
+
 def y_fold(y_coord, visible_dots):
     visible_dots_temp = visible_dots.copy()
-    for (x, y) in visible_dots:
+    for x, y in visible_dots:
         if y > y_coord:
             visible_dots_temp.add((x, y_coord - (y - y_coord)))
 
@@ -26,9 +26,10 @@ def y_fold(y_coord, visible_dots):
     visible_dots_temp.difference_update(to_discard)
     return visible_dots_temp
 
+
 def x_fold(x_coord, visible_dots):
     visible_dots_temp = visible_dots.copy()
-    for (x, y) in visible_dots:
+    for x, y in visible_dots:
         if x > x_coord:
             visible_dots_temp.add((x_coord - (x - x_coord), y))
 
@@ -36,13 +37,16 @@ def x_fold(x_coord, visible_dots):
     visible_dots_temp.difference_update(to_discard)
     return visible_dots_temp
 
+
 def part1():
     instruction = parsed_instructions[0]
+    visible_dots_p1 = visible_dots.copy()
     if instruction[0] == "x":
-        x_fold(instruction[1])
+        visible_dots_p1 = x_fold(instruction[1], visible_dots)
     else:
-        y_fold(instruction[1])
-    print(len(visible_dots))
+        visible_dots_p1 = y_fold(instruction[1], visible_dots)
+    print(len(visible_dots_p1))
+
 
 def part2(visible_dots):
     for instruction in parsed_instructions:
@@ -52,8 +56,9 @@ def part2(visible_dots):
             visible_dots = y_fold(instruction[1], visible_dots)
     x, y = zip(*visible_dots)
     plt.scatter(x, y)
-    plt.axis([-10,40,-10,8])
+    plt.axis([-10, 40, -10, 8])
     plt.savefig("temp.png")
 
 
+part1()
 part2(visible_dots)
