f95f41bcc9310dde6dbd9abbdf6042c4accb197f
Adam <97370646+anon-adam@users.noreply.github.com>
2024-11-06 20:55:36 -0500

    Day 15 Part 2

--- 2021/15.py
+++ 2021/15.py
@@ -1,12 +1,10 @@
-import sys
-from collections import deque
+import heapq
 
-cave = open("input/15", "r").read().splitlines()
-
-mr, mc = len(cave), len(cave[0])
+data = open("input/15", "r").read().splitlines()
+cave = list(list(map(int, row)) for row in data)
 
 
-def find_neighbors(cur):
+def find_neighbors(cur, mr, mc):
     (r, c) = cur
     neighbors = []
     dirs = {(0, 1), (1, 0), (-1, 0), (0, -1)}
@@ -17,45 +15,62 @@ def find_neighbors(cur):
     return neighbors
 
 
-def get_path(target, prev):
-    total_path = deque()
-    u = target
-    while u:
-        total_path.appendleft(u)
-        u = prev[u]
-    return total_path
+def find_value_of_lowest_risk_path(grid):
+    mr, mc = len(grid), len(grid[0])
+    target = (mr - 1, mc - 1)
 
-
-def find_value_of_lowest_risk_path(target):
-    distance_from_start = {(i, j): sys.maxsize for j in range(mc) for i in range(mr)}
-    prev = {(i, j): None for j in range(mc) for i in range(mr)}
+    distance_from_start = {(i, j): float("inf") for j in range(mc) for i in range(mr)}
     distance_from_start[(0, 0)] = 0
-    q = deque(distance_from_start.keys())
+    prev = {(i, j): None for j in range(mc) for i in range(mr)}
+
+    q = [(0, (0, 0))]
+    heapq.heapify(q)
+    visited = set()
+
     while q:
-        cur = min(q, key=lambda x: distance_from_start[x])
-        if cur == target:
-            break
-        q.remove(cur)
+        dist, node = heapq.heappop(q)
+        if node == target:
+            return dist
 
-        neighbors = find_neighbors(cur)
+        if node in visited:
+            continue
+
+        visited.add(node)
+
+        neighbors = find_neighbors(node, mr, mc)
         for neighbor in neighbors:
-            if neighbor in q:
-                potential_distance = distance_from_start[cur] + int(
-                    cave[neighbor[0]][neighbor[1]]
-                )
-                if potential_distance < distance_from_start[neighbor]:
-                    distance_from_start[neighbor] = potential_distance
-                    prev[neighbor] = cur
+            nr, nc = neighbor
+            potential_distance = distance_from_start[node] + int(grid[nr][nc])
+            if potential_distance < distance_from_start[neighbor]:
+                distance_from_start[neighbor] = potential_distance
+                prev[neighbor] = node
+                heapq.heappush(q, (potential_distance, neighbor))
 
-    return sum(
-        int(cave[x][y]) for (x, y) in get_path(target, prev) if not (x == 0 and y == 0)
-    )
+
+def generate_full_cave_map(cave):
+    mr, mc = len(cave), len(cave[0])
+    for _ in range(4):
+        for row in cave:
+            tail = row[-mr:]
+            row.extend((x + 1) if x < 9 else 1 for x in tail)
+
+    for _ in range(4):
+        for row in cave[-mc:]:
+            row = [(x + 1) if x < 9 else 1 for x in row]
+            cave.append(row)
+
+    return cave
 
 
 def part1():
-    target = (mr - 1, mc - 1)
-    print(find_value_of_lowest_risk_path(target))
+    print("Part 1: " + str(find_value_of_lowest_risk_path(cave)))
+
+
+def part2():
+    full_cave = generate_full_cave_map(cave)
+    print("Part 2: " + str(find_value_of_lowest_risk_path(full_cave)))
 
 
 if __name__ == "__main__":
     part1()
+    part2()
